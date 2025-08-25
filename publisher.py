1a5f04e449e122f27d2d7146fc2972ef0460c5d3
JorgeRZD <jrgortizd@gmail.com>
2025-08-24 23:38:17 -0600

    Update publisher.py with project-specific configurations and increase publish delay

--- publisher.py
+++ publisher.py
@@ -4,17 +4,19 @@ from google.cloud import pubsub_v1
 
 if __name__ == "__main__":
     # Replace 'my-project' with your project id
-    project = "my-project"
+    project = "beam-project-2025"
 
     # Replace 'my-topic' with your pubsub topic
-    pubsub_topic = "my-topic"
+    pubsub_topic = "projects/quick-processor-468404-r8/topics/Test-topic"
 
     # Replace 'my-service-account-path' with your service account path
-    path_service_account = "my-service-account-path"
+    path_service_account = (
+        r"C:\Users\JRZR\Desktop\lustrous-strand-468405-u8-5dfd46f025f4.json"
+    )
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path_service_account
 
     # Replace 'my-input-file-path' with your input file path
-    input_file = "my-input-file-path"
+    input_file = r"C:\Users\JRZR\Documents\Python Scripts\beam_project_2025\counts.csv"
 
     # create publisher
     publisher = pubsub_v1.PublisherClient()
@@ -28,4 +30,4 @@ if __name__ == "__main__":
             event_data = line  # entire line of input CSV is the message
             print("Publishing {0} to {1}".format(event_data, pubsub_topic))
             publisher.publish(pubsub_topic, event_data)
-            time.sleep(1)
+            time.sleep(2)
