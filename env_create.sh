0aad8faf7df611d0749c2da03322df3d1c124852
JorgeRZD <jrgortizd@gmail.com>
2025-08-25 23:35:27 -0600

    Add env_create.sh and processor.py for environment setup and data processing

---
+++ env_create.sh
@@ -0,0 +1,9 @@
+Python311_loc="C:/Users/JRZR/Python311/python.exe"
+env_name=mybeamproject_env
+
+echo $Python311_loc
+
+cd "C:/Users/JRZR/Documents/python_scripts/"
+##$Python311_loc -m venv $env_name
+
+source /c/Users/JRZR/Documents/python_scripts/mybeamproject_env/Scripts/activate
