6e9e703593290702b1aa3f040e3e0f655eda30b9
JorgeRZD <jrgortizd@gmail.com>
2025-08-27 13:59:57 -0600

    Update file paths in publisher.py to use forward slashes and increase publish delay

--- publisher.py
+++ publisher.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 
     # Replace 'my-service-account-path' with your service account path
     path_service_account = (
-        r"C:\Users\JRZR\Desktop\lustrous-strand-468405-u8-5dfd46f025f4.json"
+        r"C:/Users/JRZR/Desktop/lustrous-strand-468405-u8-5dfd46f025f4.json"
     )
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path_service_account
 
     # Replace 'my-input-file-path' with your input file path
-    input_file = r"C:\Users\JRZR\Documents\Python Scripts\beam_project_2025\counts.csv"
+    input_file = r"C:/Users/JRZR/Documents/python_scripts/beam_project_2025/counts.csv"
 
     # create publisher
     publisher = pubsub_v1.PublisherClient()
@@ -31,4 +31,4 @@ if __name__ == "__main__":
             event_data = line  # entire line of input CSV is the message
             print("Publishing {0} to {1}".format(event_data, pubsub_topic))
             publisher.publish(pubsub_topic, event_data)
-            time.sleep(2)
+            time.sleep(4)
